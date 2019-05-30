1ee4805b1344191aaa5ceba38bf87da761ca5bf4
fishyfriend <fishyfriend@users.noreply.github.com>
2019-05-30 03:20:03 -0400

    1.2 example: counting change

--- section-1.2.scm
+++ section-1.2.scm
@@ -84,3 +84,49 @@ process.
 (h n) computes P(n) where P(n) = 2 if n = 1, and 2^P(n-1) otherwise.
 More concise answer using Knuth up-arrow notation: (h n) computes 2↑↑n.
 |#
+
+;; Example: Counting change ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define (count-change amount)
+  (cc amount 5))
+
+(define (cc amount kinds-of-coins)
+  (cond ((= amount 0) 1)
+        ((or (< amount 0) (= kinds-of-coins 0)) 0)
+        (else (+ (cc amount
+                     (- kinds-of-coins 1))
+                 (cc (- amount
+                        (first-denomination kinds-of-coins))
+                      kinds-of-coins)))))
+
+(define (first-denomination kinds-of-coins)
+  (cond ((= kinds-of-coins 1) 1)
+        ((= kinds-of-coins 2) 5)
+        ((= kinds-of-coins 3) 10)
+        ((= kinds-of-coins 4) 25)
+        ((= kinds-of-coins 5) 50)))
+
+#|
+Here is a more efficient version without using memoization. It was inspired by
+the observation most kinds of coin are exact multiples of each other: one
+half-dollar equals two quarters, one quarter equals five nickels, and one nickel
+equals five pennies. One can use this information to build an efficient
+recursive algorithm for calculating change using only this limited subset of
+coins, then add in special handling for dimes.
+|#
+
+(define (cc kind n-coins n-subcoins)
+  (define n-unused-subcoins
+    (- n-subcoins (* n-coins (if (= kind 5) 2 5))))
+  (if (or (> kind 5) (< n-unused-subcoins 0))
+      0
+      (+ 1 (cc kind (+ n-coins 1) n-subcoins)
+           (cc (+ kind (if (= kind 2) 2 1)) 1 n-coins)
+           (cond ((= kind 2) (quotient n-coins 2))
+                 ((and (= kind 4) (> n-coins 0))
+                   (* (quotient n-unused-subcoins 2)
+                      (+ 1 (quotient n-coins 2))))
+                 (else 0)))))
+
+(define (count-change amount)
+  (if (< amount 0) 0 (+ 1 (cc 2 1 amount))))
