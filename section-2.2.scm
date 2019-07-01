37350c165e832003a6e97715ef46421d31fc7739
fishyfriend <fishyfriend@users.noreply.github.com>
2019-07-01 06:02:21 -0400

    2.2 exx 24-29

--- section-2.2.scm
+++ section-2.2.scm
@@ -99,3 +99,147 @@ nested pair of the form (cons (cons (cons '() item1) item2) item3). |#
       #t
       (begin (f (car xs))
              (for-each f (cdr xs)))))
+
+;; Exercise 2.24 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(list 1 (list 2 (list 3 4)))
+
+#| Interpreter result:
+
+  (1 (2 (3 4)))
+
+Box-and-pointer structure:
+
+  (1 (2 (3 4)))──>[•][•]────>[•|/]
+                   ↓          ↓
+                  [1]  ┌───>[•|•]─────>[•|/]
+                       │     ↓          ↓
+                (2 (3 4))   [2]  ┌───>[•|•] → [•|/]
+                                 │     ↓       ↓
+                              (3 4)   [3]     [4]
+
+Tree:
+
+  (1 (2 (3 4)))
+    ├─ 1
+    └─ (2 (3 4))
+         ├─ 2
+         └─ (3 4)
+              ├─ 3
+              └─ 4 |#
+
+;; Exercise 2.25 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define x '(1 3 (5 7) 9))
+(car (cdr (car (cdr (cdr x))))) ; 7
+
+(define y '((7)))
+(car (car y)) ; 7
+
+(define z '(1 (2 (3 (4 (5 (6 7)))))))
+(car (cdr (car (cdr (car (cdr (car (cdr (car (cdr (car (cdr z)))))))))))) ; 7
+
+;; Exercise 2.26 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define x (list 1 2 3))
+(define y (list 4 5 6))
+
+(append x y) ; (1 2 3 4 5 6)
+(cons x y) ; ((1 2 3) 4 5 6)
+(list x y) ; ((1 2 3) (4 5 6))
+
+;; Exercise 2.27 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define (deep-reverse item)
+  (define (iter old new)
+    (if (null? old)
+        new
+        (iter (cdr old)
+              (cons (deep-reverse (car old))
+                    new))))
+  (if (list? item)
+      (iter item '())
+      item))
+
+(define x (list (list 1 2) (list 3 4)))
+x ; ((1 2) (3 4))
+(reverse x) ; ((3 4) (1 2))
+(deep-reverse x) ; ((4 3) (2 1))
+
+;; Exercise 2.28 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define (fringe tree)
+  (cond ((null? tree) '())
+        ((not (list? tree)) (list tree))
+        (else (append (fringe (car tree))
+                      (fringe (cdr tree))))))
+
+(define x (list (list 1 2) (list 3 4)))
+(fringe x) ; (1 2 3 4)
+(fringe (list x x)) ; (1 2 3 4 1 2 3 4)
+
+;; Exercise 2.29 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define (make-mobile left right)
+  (list left right))
+
+(define (make-branch length structure)
+  (list length structure))
+
+; a.
+
+(define (left-branch mobile) (car mobile))
+(define (right-branch mobile) (cadr mobile))
+(define (branch-length branch) (car branch))
+(define (branch-structure branch) (cadr branch))
+
+; b.
+
+(define (total-weight mobile)
+  (define (weight branch)
+    (let ((structure (branch-structure branch)))
+      (if (number? structure)
+          structure
+          (total-weight structure))))
+  (+ (weight (left-branch mobile)) (weight (right-branch mobile))))
+
+(total-weight
+  (make-mobile (make-branch 5 20)
+               (make-branch 6 (make-mobile (make-branch 7 30)
+                                           (make-branch 8 40))))) ; 90
+
+; c.
+
+(define (balanced? mobile)
+  (define (weight branch)
+    (let ((structure (branch-structure branch)))
+      (if (number? structure)
+          structure
+          (total-weight structure))))
+  (let ((left (left-branch mobile))
+        (right (right-branch mobile)))
+    (= (* (branch-length left) (weight left))
+       (* (branch-length right) (weight right)))))
+
+(balanced?
+  (make-mobile (make-branch 5 20)
+               (make-branch 6 (make-mobile (make-branch 7 30)
+                                           (make-branch 8 40))))) ; #f
+
+(balanced?
+  (make-mobile (make-branch 7 3)
+               (make-branch 3 (make-mobile (make-branch 4 5)
+                                           (make-branch 10 2))))) ; #t
+
+; d.
+
+(define (make-mobile left right)
+  (cons left right))
+
+(define (make-branch length structure)
+  (cons length structure))
+
+#| To adapt to this new representation, only the selectors left-branch,
+right-branch, branch-length, and branch-structure need be updated. All the other
+procedures are implemented in terms of these and do not access the underlying
+structure directly. |#
