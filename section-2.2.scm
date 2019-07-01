04f75a23e89a0f617db29b4d19a620bd88bdfc32
fishyfriend <fishyfriend@users.noreply.github.com>
2019-07-01 16:03:28 -0400

    2.2 exx 30-39

--- section-2.2.scm
+++ section-2.2.scm
@@ -243,3 +243,173 @@ x ; ((1 2) (3 4))
 right-branch, branch-length, and branch-structure need be updated. All the other
 procedures are implemented in terms of these and do not access the underlying
 structure directly. |#
+
+;; Exercise 2.30 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define (square-tree tree)
+  (if (null? tree)
+      tree
+      (cons (if (number? (car tree))
+                (square (car tree))
+                (square-tree (car tree)))
+            (square-tree (cdr tree)))))
+
+(define (square-tree tree)
+  (map (lambda (item)
+         ((if (number? item)
+              square
+              square-tree)
+          item))
+       tree))
+
+(square-tree
+  (list 1
+        (list 2 (list 3 4) 5)
+        (list 6 7))) ; (1 (4 (9 16) 25) (36 49))
+
+;; Exercise 2.31 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define (tree-map f tree)
+  (if (null? tree)
+      tree
+      (cons (if (list? (car tree))
+                (tree-map f (car tree))
+                (f (car tree)))
+            (tree-map f (cdr tree)))))
+
+(define (square-tree tree) (tree-map square tree))
+
+;; Exercise 2.32 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define (subsets s)
+  (if (null? s)
+      (list '())
+      (let ((rest (subsets (cdr s))))
+        (append rest
+                (map (lambda (ss) (cons (car s) ss))
+                     rest)))))
+
+(subsets (list 1 2 3)) ; (() (3) (2) (2 3) (1) (1 3) (1 2) (1 2 3))
+
+#| This implementation works based on the following rationale. A subset of set S
+is a combination of zero or more elements of S. To find all subsets of S we must
+find all possible combinations of elements in S. For empty S the only subset is
+is the empty set, the only possible combination of zero elements. For non-empty
+S, observe that for any element e in S, the possible subsets of S are the union
+of all combinations that include e and those that don't. Observe also that to
+get the combinations that include e, we can simply add e to every other possible
+combination. All that remains is to calculate the combinations that do not
+include e, which is simply done by applying recursively the logic just laid out.
+|#
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (accumulate op initial (cdr sequence)))))
+
+;; Exercise 2.33 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define (map p sequence)
+  (accumulate (lambda (x y) (cons (p x) y)) '() sequence))
+
+(define (append seq1 seq2)
+  (accumulate cons seq2 seq1))
+
+(define (length sequence)
+  (accumulate (lambda (x y) (+ y 1)) 0 sequence))
+
+;; Exercise 2.34 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define (horner-eval x coefficient-sequence)
+  (accumulate (lambda (this-coeff higher-terms)
+                (+ this-coeff (* x higher-terms)))
+              0
+              coefficient-sequence))
+
+(horner-eval 2 (list 1 3 0 5 0 1)) ; 79
+
+;; Exercise 2.35 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define (count-leaves t)
+  (accumulate + 0 (map (lambda (x)
+                         (if (list? x)
+                             (count-leaves x)
+                             1))
+                       t)))
+
+;; Exercise 2.36 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define (accumulate-n op init seqs)
+  (if (null? (car seqs))
+      '()
+      (cons (accumulate op init (map car seqs))
+            (accumulate-n op init (map cdr seqs)))))
+
+(accumulate-n + 0 '((1 2 3) (4 5 6) (7 8 9) (10 11 12))) ; (22 26 30)
+
+;; Exercise 2.37 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+; n.b. must use the stdlib definition of map
+
+(define (dot-product v w)
+  (accumulate + 0 (map * v w)))
+
+(define (matrix-*-vector m v)
+  (map (lambda (row) (dot-product row v))
+       m))
+
+(define (transpose mat)
+  (accumulate-n cons '() mat))
+
+(define (matrix-*-matrix m n)
+  (let ((cols (transpose n)))
+    (map (lambda (row)
+           (accumulate cons '() (map (lambda (col)
+                                       (dot-product row col))
+                                     cols)))
+         m)))
+
+(define v1 '(1 3 5 7))
+(define v2 '(2 4 6 8))
+(define m1 '((1 2 3 4) (4 5 6 6) (6 7 8 9)))
+(define m2 '((1 2 3) (3 3 4) (4 5 6) (7 8 8)))
+(define m3 '((1 2) (3 5) (4 5) (6 9)))
+
+(dot-product v1 v2) ; 100
+(matrix-*-vector m1 v1) ; (50 91 130)
+(transpose m1) ; ((1 4 6) (2 5 7) (3 6 8) (4 6 9))
+(matrix-*-matrix m1 m2) ; ((47 55 61) (85 101 116) (122 145 166))
+(matrix-*-matrix m1 m3) ; ((43 63) (79 117) (113 68))
+
+;; Exercise 2.38 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define fold-right accumulate)
+
+(define (fold-left op initial sequence)
+  (define (iter result rest)
+    (if (null? rest)
+        result
+        (iter (op result (car rest))
+              (cdr rest))))
+  (iter initial sequence))
+
+(fold-right / 1 (list 1 2 3)) ; 3/2
+(fold-left / 1 (list 1 2 3)) ; 1/6
+(fold-right list '() (list 1 2 3)) ; (1 (2 (3 ())))
+(fold-left list '() (list 1 2 3)) ; (((() 1) 2) 3)
+
+#| To guarantee that fold-right and fold-left produce the same result for every
+sequence, (op a b) should be equivalent to (op b a). |#
+
+;; Exercise 2.39 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define (reverse sequence)
+  (fold-right (lambda (x y) (append y (list x))) '() sequence))
+
+(define (reverse sequence)
+  (fold-left (lambda (x y) (cons y x)) '() sequence))
+
+(reverse '(1 2 3)) ; (3 2 1)
