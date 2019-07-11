8f59d2b3f0c6a82b140474f3ceaecf8f10b7a15a
fishyfriend <fishyfriend@users.noreply.github.com>
2019-07-10 20:16:28 -0400

    migrate ch2 exx 75-76

--- ch2.scm
+++ ch2.scm
@@ -2982,6 +2982,45 @@
 ;   - Ensure the new division's files are accessible on the company-wide shared
 ;     filesystem.
 
+;; EXERCISE 2.75
+(define (make-from-mag-ang x y)
+  (define (dispatch op)
+    (cond ((eq? op 'real-part) (* (magnitude z) (cos (angle z))))
+          ((eq? op 'imag-part) (* (magnitude z) (sin (angle z))))
+          ((eq? op 'magnitude) (car z))
+          ((eq? op 'angle) (cdr z))
+          (else
+            (error "Unknown op -- MAKE-FROM-MAG-ANG" op))))
+  dispatch)
+
+;; EXERCISE 2.76
+; For generic operations with explicit dispatch, adding a new type requires
+; modifying every generic procedure to handle the new type. Adding a new
+; operation requires implementing a single procedure with logic to handle all
+; existing types.
+;
+; For data-directed style, adding a new type requires writing a procedure to
+; implement every existing operation for the new type, as well as creating an
+; installation procedure which registers these procedures. Adding a new
+; operation requires writing procedures to implement that operation for all
+; existing types, and updating all existing types' registration code to register
+; those procedures.
+;
+; For message-passing style, adding a new type requires implementing a single
+; procedure with logic to handle all existing operations. Adding a new operation
+; requires modifying all existing types' implementation procedures to handle the
+; new operation.
+;
+; In a system where new types must often be added, the data-directed and
+; message-passing styles are better suited, as the addition of a new type does
+; not require changes to existing code. (The data-directed style may be slightly
+; nicer than message-passing because it cleanly separates the dispatch logic
+; from the application logic.)
+;
+; If new operations must often be added, then generic operations with explicit
+; dispatch will be best, for a similar reason -- existing code does not have to
+; move to accommodate the new operations.
+
 
 ;; Message passing
 (define (make-from-real-imag x y)
