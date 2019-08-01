b9d921f0bd9ced58bbd618f49f28cdfad5c3b15d
fishyfriend <fishyfriend@users.noreply.github.com>
2019-08-01 17:59:31 -0400

    ch 3 exx 3.59-60

--- ch3.scm
+++ ch3.scm
@@ -3071,8 +3071,29 @@
 
 
 ;; EXERCISE 3.59
+;; a.
+(define (integrate-series s)
+  (mul-streams (stream-map (lambda (n) (/ 1 n))
+                          integers)
+               s))
+
+;; b.
 ;: (define exp-series
 ;:   (cons-stream 1 (integrate-series exp-series)))
+
+(define cosine-series
+  (cons-stream 1 (integrate-series (scale-stream sine-series -1))))
+
+(define sine-series
+  (cons-stream 0 (integrate-series cosine-series)))
+
+
+;; EXERCISE 3.60
+(define (mul-series s1 s2)
+  (cons-stream
+    (* (stream-car s1) (stream-car s2))
+    (add-streams (scale-stream (stream-cdr s2) (stream-car s1))
+                 (mul-series (stream-cdr s1) s2))))
 
 
 ;;;SECTION 3.5.3
