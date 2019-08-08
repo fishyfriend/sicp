f14f5c64699111c263431c12ee467bb2fac9eb07
fishyfriend <fishyfriend@users.noreply.github.com>
2019-08-08 04:01:19 -0400

    ch 3 ex 77

--- ch3.scm
+++ ch3.scm
@@ -3496,15 +3496,26 @@
 
 
 ;; EXERCISE 3.77
+;: (define (integral integrand initial-value dt)
+;:   (cons-stream initial-value
+;:                (if (stream-null? integrand)
+;:                    the-empty-stream
+;:                    (integral (stream-cdr integrand)
+;:                              (+ (* dt (stream-car integrand))
+;:                                 initial-value)
+;:                              dt))))
 
-(define (integral integrand initial-value dt)
-  (cons-stream initial-value
-               (if (stream-null? integrand)
-                   the-empty-stream
-                   (integral (stream-cdr integrand)
-                             (+ (* dt (stream-car integrand))
-                                initial-value)
-                             dt))))
+(define (integral delayed-integrand initial-value dt)
+  (cons-stream
+    initial-value
+    (let ((integrand (force delayed-integrand)))
+      (if (stream-null? integrand)
+          the-empty-stream
+          (integral (delay (stream-cdr integrand))
+                    (+ (* dt (stream-car integrand))
+                       initial-value)
+                    dt)))))
+
 
 ;;;SECTION 3.5.5
 
