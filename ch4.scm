c42ff7c671f6c2eb9e5dc644337553936019d9d8
fishyfriend <fishyfriend@users.noreply.github.com>
2019-09-01 12:37:47 -0400

    ch 4 exx 61-63

--- ch4.scm
+++ ch4.scm
@@ -3522,18 +3522,35 @@
 
 
 ;; EXERCISE 4.61
-
 (rule (?x next-to ?y in (?x ?y . ?u)))
 
 (rule (?x next-to ?y in (?v . ?z))
       (?x next-to ?y in ?z))
 
+;;; Query input:
 (?x next-to ?y in (1 (2 3) 4))
+;;; Query response:
+(1 next-to (2 3) in (1 (2 3) 4))
+((2 3) next-to 4 in (1 (2 3) 4))
+
+;;; Query input:
 (?x next-to 1 in (2 1 3 1))
+;;; Query response:
+(2 next-to 1 in (2 1 3 1))
+(3 next-to 1 in (2 1 3 1))
+
+
+;; EXERCISE 4.62
+(rule (last-pair (?x) ?x))
+(rule (last-pair (?x . ?xs) ?y) (last-pair ?xs ?y))
+
+;; A query like (last-pair ?x (3)) appears to produce an infinite recursion, as
+;; evaluation aborts with the error that maximum recursion depth was exceeded.
+;; The query system does not have enough information to produce a finite set of
+;; results.
 
 
 ;; EXERCISE 4.63
-
 (son Adam Cain)
 (son Cain Enoch)
 (son Enoch Irad)
@@ -3543,6 +3560,14 @@
 (wife Lamech Ada)
 (son Ada Jabal)
 (son Ada Jubal)
+
+(rule (grandson ?g ?s)
+  (and (son ?f ?s)
+       (son ?g ?f)))
+
+(rule (son ?m ?s)
+  (and (wife ?m ?w)
+       (son ?w ?s)))
 
 
 ;;;SECTION 4.4.2
--- ch4tests.scm
+++ ch4tests.scm
@@ -733,3 +733,39 @@ try-again
      (lisp-value in-order? (fect cy d) (hacker alyssa p)))
 (and (lives-near (bitdiddle ben) (reasoner louis))
      (lisp-value in-order? (bitdiddle ben) (reasoner louis)))
+
+
+;; EXERCISE 4.62
+;;; Query input:
+(last-pair (3) ?x)
+;;; Query results:
+(last-pair (3) 3)
+
+;;; Query input:
+(last-pair (1 2 3) ?x)
+;;; Query results:
+(last-pair (1 2 3) 3)
+
+;;; Query input:
+(last-pair (2 ?x) (3))
+;;; Query results:
+(last-pair (2 (3)) (3))
+
+
+;; EXERCISE 4.63
+;;; Query input:
+(grandson Cain ?x)
+;;; Query response:
+(grandson cain irad)
+
+;;; Query input:
+(son Lamech ?x)
+;;; Query response:
+(son lamech jubal)
+(son lamech jabal)
+
+;;; Query input:
+(grandson Methushael ?x)
+;;; Query response:
+(grandson methushael jubal)
+(grandson methushael jabal)
